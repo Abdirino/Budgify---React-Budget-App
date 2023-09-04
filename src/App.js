8ed0bc720c44066d52611689cae5e06d02ef5f36
Abdirino <abdirinoahash@gmail.com>
2023-09-04 10:23:06 +0300

    i'm back commit . first styles of budgify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Container, Stack, Button } from 'react-bootstrap';
+import BudgetCard from './Components/BudgetCard';
 
 function App() {
   return (
     <div className="App">
-      <h1>React Budget App</h1>
+      <Container className='my-4'>
+        <Stack direction='horizontal' gap="2" className='mb-4'>
+          <h1 className='me-auto'>Bud<span className='text-success'>gify</span></h1>
+          <Button variant='success'>Add Budget</Button>
+          <Button variant='outline-success'>Add Expenses</Button>
+        </Stack>
+        <div style={{ display: "grid", gridTemplateColumns: "repeat()auto-fill, minmax(300px, 1fr)", gap: '1.2rem', alignItems: 'flex-start' }}>
+          <BudgetCard name='Entertainment' amount={200} max={1000}></BudgetCard>
+        </div>
+      </Container>
     </div>
   );
 }
