e0bc6c520765d3baf618148c6dbd976e52277fdf
Abdirino <abdirinoahash@gmail.com>
2023-09-05 13:48:12 +0300

    still erred

--- src/contexts/BudgetContext.jsx
+++ src/contexts/BudgetContext.jsx
@@ -1,5 +1,9 @@
-export function useBudget() {
+import React, { useContext } from 'react';
 
+const BudgetContext = React.createContext()
+
+export function useBudget() {
+    return useContext(BudgetContext)
 }
 export const BudgetProvider = ({children}) => {
     return children
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
-import { BudgetContext } from './contexts';
+import { BudgetProvider } from './Contexts/BudgetContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BudgetProvider>
+      <App />
+    </BudgetProvider>
   </React.StrictMode>
 );
 
